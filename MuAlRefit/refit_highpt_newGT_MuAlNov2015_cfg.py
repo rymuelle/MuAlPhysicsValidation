3fc8ead8c9f2920a1a60cd30d970287df426e3ae
Aysen Tatarinov <aysen.tatarinov@cern.ch>
2015-11-30 17:48:35 +0100

    initial commit of MuAlRefit

---
+++ MuAlRefit/refit_highpt_newGT_MuAlNov2015_cfg.py
@@ -0,0 +1,107 @@
+import FWCore.ParameterSet.Config as cms 
+import os
+
+ijob = int(os.environ["IJOB"])
+input_files = os.environ["INPUTFILES"].split(" ")
+
+process = cms.Process("MUALREFIT")
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(*input_files)
+)
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+
+process.MessageLogger = cms.Service("MessageLogger",
+                                    destinations = cms.untracked.vstring("cout"),
+                                    cout = cms.untracked.PSet(threshold = cms.untracked.string("ERROR"),
+                                                              ERROR = cms.untracked.PSet( limit = cms.untracked.int32(10) )
+                                                             )
+                                   )
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+process.GlobalTag.globaltag = "74X_dataRun2_Candidate_2015_11_18_10_21_42"
+
+process.load("Configuration.Geometry.GeometryExtended2015Reco_cff")
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("RecoTracker.TrackProducer.TrackRefitter_cfi")
+process.load("RecoTracker.MeasurementDet.MeasurementTrackerEventProducer_cfi")
+
+process.MeasurementTrackerEvent = process.MeasurementTrackerEvent.clone()
+
+process.muAlGeneralTracks = process.TrackRefitter.clone()
+
+process.muAlAncientMuonSeed = process.ancientMuonSeed.clone()
+
+process.muAlStandAloneMuons = process.standAloneMuons.clone()
+# this line switch on "old" hit based muon reconstruction
+#process.muAlStandAloneMuons.STATrajBuilderParameters.BWFilterParameters.MuonTrajectoryUpdatorParameters.Granularity = 2
+process.muAlStandAloneMuons.InputObjects = cms.InputTag("muAlAncientMuonSeed")
+
+process.muAlGlobalMuons = process.globalMuons.clone()
+process.muAlGlobalMuons.TrackerCollectionLabel = cms.InputTag("muAlGeneralTracks")
+process.muAlGlobalMuons.MuonCollectionLabel = cms.InputTag("muAlStandAloneMuons","UpdatedAtVtx")
+
+process.muAlTevMuons = process.tevMuons.clone()
+process.muAlTevMuons.MuonCollectionLabel = cms.InputTag("muAlGlobalMuons")
+
+process.muAlGlbTrackQual = process.glbTrackQual.clone()
+process.muAlGlbTrackQual.InputCollection = cms.InputTag("muAlGlobalMuons")
+process.muAlGlbTrackQual.InputLinksCollection = cms.InputTag("muAlGlobalMuons")
+
+process.muAlMuons = process.muons1stStep.clone()
+process.muAlMuons.inputCollectionTypes = cms.vstring('inner tracks','links','outer tracks','tev firstHit','tev picky','tev dyt')
+process.muAlMuons.inputCollectionLabels = cms.VInputTag( cms.InputTag("muAlGeneralTracks"),
+                                                         cms.InputTag("muAlGlobalMuons"),
+                                                         cms.InputTag("muAlStandAloneMuons","UpdatedAtVtx"),
+                                                         cms.InputTag("muAlTevMuons","firstHit"),
+                                                         cms.InputTag("muAlTevMuons","picky"),
+                                                         cms.InputTag("muAlTevMuons","dyt")
+                                                       )
+
+process.muAlMuons.fillGlobalTrackQuality = cms.bool(True)
+process.muAlMuons.globalTrackQualityInputTag = cms.InputTag('muAlGlbTrackQual')
+#process.muAlMuons.fillGlobalTrackRefits = cms.bool(False)
+
+# This is to load new CondDB
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
+# Muon geometry
+process.muonDtAlignment = cms.ESSource("PoolDBESSource", CondDBSetup,
+                                     connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+                                     toGet   = cms.VPSet(cms.PSet(record = cms.string("DTAlignmentRcd"),  tag = cms.string("DTAlignment_Nov2015_7412p4_data-Run2015D-Prompt-v3_TB110001_v1")))
+                                     )
+process.es_prefer_muonDtAlignment = cms.ESPrefer("PoolDBESSource","muonDtAlignment")
+
+process.muonCscAlignment = cms.ESSource("PoolDBESSource", CondDBSetup,
+                                     connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+                                     toGet   = cms.VPSet(cms.PSet(record = cms.string("CSCAlignmentRcd"), tag = cms.string("CSCAlignment_Nov2015_7412p4_data-Run2015D-Prompt-v3_RingsMoveGlobal-TB100001_v1")))
+                                     )
+process.es_prefer_muonCscAlignment = cms.ESPrefer("PoolDBESSource","muonCscAlignment")
+
+process.globalPosition = cms.ESSource("PoolDBESSource", CondDBSetup,
+                                     connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+                                     toGet   = cms.VPSet(cms.PSet(record = cms.string("GlobalPositionRcd"), tag = cms.string("GlobalPosition_MuonGPR_Nov2015_74x_data-Run2015D-Prompt-v3v4_v1")))
+                                     )
+process.es_prefer_globalPosition = cms.ESPrefer("PoolDBESSource","globalPosition")
+
+process.muonPtFilter = cms.EDFilter("MuonPtFilter")
+
+process.Path = cms.Path(process.muonPtFilter * process.MeasurementTrackerEvent * process.muAlGeneralTracks * process.muAlAncientMuonSeed * process.muAlStandAloneMuons * process.muAlGlobalMuons * process.muAlTevMuons * process.muAlGlbTrackQual * process.muAlMuons)
+
+process.out = cms.OutputModule("PoolOutputModule",
+                                outputCommands = cms.untracked.vstring("drop *",
+                                                                       "keep GenEventInfoProduct_generator_*_*",
+                                                                       "keep edmHepMCProduct_generator_*_*",
+                                                                       "keep *_genParticles_*_*",
+                                                                       "keep recoBeamSpot_offlineBeamSpot_*_*",
+                                                                       "keep *_TriggerResults_*_*",
+                                                                       "keep recoMuons_*_*_MUALREFIT",
+                                                                       "keep recoTracks_*_*_MUALREFIT",
+                                                                       "keep recoTrackExtras_*_*_MUALREFIT"
+                                                                       ),
+                                fileName = cms.untracked.string("output_%03d.root" % ijob),
+                                SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring("Path")),
+                                )
+
+process.EndPath = cms.EndPath(process.out)
